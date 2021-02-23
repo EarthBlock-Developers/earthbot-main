c21f7e024a122944aed966c1dca33e5aa955d494
Sachsenspielt96 <59836848+Sachsenspielt96@users.noreply.github.com>
2021-02-23 18:36:34 +0100

    security change for public

--- settings.js
+++
@@ -1,8 +0,0 @@
-module.exports.variables = {
-  "TOKEN": {
-    "DISCORD": "ODA3OTg0MzQxNTE3ODYwODc0.YB_8Dg.4_GZVjzw7BMrZeel02bdNi684xs",
-    "PTERODACTYL": "BwELET7P7BbEdwnB0O1K8fNUFAmvOacZMvY4CUITWpMnrTwZ"
-  },
-  "PREFIX": "eb!",
-  "SERVICE": false
-}
